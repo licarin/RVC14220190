538a9142411eea9975b4aaab8be8feb0e3001880
Bill <136879108+licarin@users.noreply.github.com>
2024-11-13 16:13:23 +0700

    update

--- app/src/main/java/com/paba/recyclerview/MainActivity.kt
+++ app/src/main/java/com/paba/recyclerview/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        _rvWayang = findViewById(R.id.rvWayang)
+        SiapkanData()
+        TambahData()
+        TampilkanData()
+    }
+
+    private lateinit var  _nama: Array<String>
+    private lateinit var  _karakter: Array<String>
+    private lateinit var  _deskripsi: Array<String>
+    private lateinit var  _gambar: Array<String>
+    private lateinit var _rvWayang : RecyclerView
+
+    private var arWayang = arrayListOf<wayang>()
+
+    fun SiapkanData(){
+        _nama = resources.getStringArray(R.array.namaWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _gambar = resources.getStringArray(R.array.gambarWayang)
+    }
+
+    fun TambahData(){
+        for (position in  _nama.indices){
+            val data = wayang(
+                _gambar[position],
+                _nama[position],
+                _karakter[position],
+                _deskripsi[position]
+            )
+            arWayang.add(data)
+        }
+    }
+
+    fun TampilkanData(){
+        _rvWayang.layoutManager = LinearLayoutManager(this)
+        _rvWayang.adapter = adapterRecView(arWayang)
     }
 }
