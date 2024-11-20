57ac57d8cad1a78e433034edc6503e1344067534
Bill <136879108+licarin@users.noreply.github.com>
2024-11-20 18:41:11 +0700

    update

--- app/src/main/java/com/paba/recyclerview/MainActivity.kt
+++ app/src/main/java/com/paba/recyclerview/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.paba.recyclerview
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _nama: Array<String>
+    private lateinit var _karakter: Array<String>
+    private lateinit var _deskripsi: Array<String>
+    private lateinit var _gambar: Array<String>
+    private lateinit var _rvWayang: RecyclerView
+
+    private var arwayang = arrayListOf<wayang>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,41 +30,53 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        _rvWayang = findViewById(R.id.rvWayang)
-        SiapkanData()
-        TambahData()
-        TampilkanData()
+
+        _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
+        siapkanData()
+        tambahData()
+        tampilkanData()
     }
 
-    private lateinit var  _nama: Array<String>
-    private lateinit var  _karakter: Array<String>
-    private lateinit var  _deskripsi: Array<String>
-    private lateinit var  _gambar: Array<String>
-    private lateinit var _rvWayang : RecyclerView
-
-    private var arWayang = arrayListOf<wayang>()
-
-    fun SiapkanData(){
+    fun siapkanData(){
         _nama = resources.getStringArray(R.array.namaWayang)
-        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
         _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
         _gambar = resources.getStringArray(R.array.gambarWayang)
     }
 
-    fun TambahData(){
-        for (position in  _nama.indices){
+    fun tambahData(){
+        for (position: Int in _nama.indices){
             val data = wayang(
                 _gambar[position],
                 _nama[position],
                 _karakter[position],
                 _deskripsi[position]
             )
-            arWayang.add(data)
+
+            arwayang.add(data)
         }
     }
 
-    fun TampilkanData(){
-        _rvWayang.layoutManager = LinearLayoutManager(this)
-        _rvWayang.adapter = adapterRecView(arWayang)
+    fun tampilkanData(){
+        _rvWayang.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+//        _rvWayang.adapter = adapterRecView(arwayang)
+
+        val adapterWayang = adapterRecView(arwayang)
+        _rvWayang.adapter = adapterWayang
+
+        adapterWayang.setOnItemClickCallback(object : adapterRecView.OnItemClickCallback{
+            override fun onItemClicked(data: wayang) {
+                Toast.makeText(this@MainActivity,data.nama, Toast.LENGTH_LONG)
+                    .show()
+
+                val intent = Intent(this@MainActivity,detWayang::class
+                    .java)
+
+                intent.putExtra("kirimData", data)
+                startActivity(intent)
+            }
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/paba/recyclerview/adapterRecView.kt
@@ -0,0 +1,64 @@
+package com.paba.recyclerview
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class adapterRecView(private val listwayang: ArrayList<wayang>): RecyclerView
+.Adapter<adapterRecView.ListViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): adapterRecView.ListViewHolder {
+        val view : View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recycler, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: adapterRecView.ListViewHolder, position: Int) {
+        var wayang: wayang = listwayang[position]
+
+        holder._namaWayang.setText(wayang.nama)
+        holder._deskripsiWayang.setText(wayang.deskripsi)
+        holder._karakterWayang.setText(wayang.karakter)
+        Log.d("TEST", wayang.foto)
+        Picasso.get()
+            .load(wayang.foto)
+            .into(holder._fotoWayang)
+
+        holder._fotoWayang.setOnClickListener {
+            Toast.makeText(holder.itemView.context, wayang.nama, Toast.LENGTH_LONG).show()
+        }
+
+        holder._fotoWayang.setOnClickListener{
+            onItemClickCallback.onItemClicked(listwayang[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listwayang.size
+    }
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data:wayang)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    inner class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var _namaWayang = itemView.findViewById<TextView>(R.id.namaWayang)
+        var _karakterWayang = itemView.findViewById<TextView>(R.id.karaterWayang)
+        var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
+        var _fotoWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
+    }
+}
